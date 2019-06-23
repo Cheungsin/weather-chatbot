ca70351e9a9508efb5330e5e746af87ee3c52eff
杨承欣 <chengxin@yangcheeMacBook.lan>
2019-06-24 02:35:42 +0800

    Add Rasa and data

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def initInterPreter():
     trainer = Trainer(config.load("config_spacy.yml"))
     
     # Load the training data
-    training_data = load_data('training_dataset9.json')
+    training_data = load_data('training_dataset14.json')
     
     # Create an interpreter by training the model
     globleDict['interpreter'] = trainer.train(training_data)
@@ -105,30 +105,40 @@ def getOneDayData(weatherData, day): # 参数：API获得的数据，希望获�
     #print(weatherData)
     if (day == -1):
         data = weatherData['data']['yesterday']
-        data['date'] = weatherData['data']['city'] + '昨日(' + data['date']
+        #data['date'] = 'The weather condition of ' + weatherData['data']['city'] + ' on yesterday(' + data['date'] + ') is as follows:'
+        data['date'] = '{} yesterday({})'.format(weatherData['data']['city'], data['date'])
+        data['isgoing'] = False
     else:
         data = weatherData['data']['forecast'][day]
         if (day == 0):
-            data['date'] = weatherData['data']['city'] + '今日(' + data['date']
+            data['date'] = '{} today({})'.format(weatherData['data']['city'], data['date'])
+            data['isgoing'] = False
+            #data['date'] = 'The weather condition of ' + weatherData['data']['city'] + ' on today(' + data['date'] + ') will be as follows:'
         elif (day == 1):
-            data['date'] = weatherData['data']['city'] + '明日(' + data['date']
+            data['date'] = '{} tomorrow({})'.format(weatherData['data']['city'], data['date'])
+            data['isgoing'] = True
         elif (day == 2):
-            data['date'] = weatherData['data']['city'] + '后天(' + data['date']
+            data['date'] = '{} the day after tomorrow({})'.format(weatherData['data']['city'], data['date'])
+            data['isgoing'] = True
         else:
-            data['date'] = weatherData['data']['city'] + str(day) + '天后(' + data['date']
+            #data['date'] = weatherData['data']['city'] + str(day) + '天后(' + data['date']
+            data['date'] = '{} {} days later({})'.format(weatherData['data']['city'], str(day), data['date'])
+            data['isgoing'] = True
     #print(data)
     return data
 #getOneDayData(getWeather("shenzhen"), -1)
 
 def parseOneDayStr(oneDayData, style):
     if style == 'all':
-        stri = oneDayData['date'] + ')天气:' + '\n' + oneDayData['low'] + ', ' + oneDayData['high'] + '\n' + oneDayData['type'] + '\n' + (oneDayData['fx'] if 'fx' in oneDayData else oneDayData['fengxiang']) + ', ' + re.search(r'(?<=CDATA\[).*(?=\]\])', (oneDayData['fl'] if 'fl' in oneDayData else oneDayData['fengli'])).group()
+        #stri = oneDayData['date'] + ')天气:' + '\n' + oneDayData['low'] + ', ' + oneDayData['high'] + '\n' + oneDayData['type'] + '\n' + (oneDayData['fx'] if 'fx' in oneDayData else oneDayData['fengxiang']) + ', ' + re.search(r'(?<=CDATA\[).*(?=\]\])', (oneDayData['fl'] if 'fl' in oneDayData else oneDayData['fengli'])).group()
+        stri = 'The weather condition of {} will be as follows: the highest temperature will be {}℃  and the lowest temperature will be {}℃ . And it will be "{}" at that time.'.format(oneDayData['date'], re.search(r'\d+', oneDayData['high']).group(), re.search(r'-?\d+', oneDayData['low']).group(), oneDayData['type'])
     elif style == 'onlyTemperature':
-        stri = oneDayData['date'] + ')温度:' + '\n' + oneDayData['low'] + ', ' + oneDayData['high']
+        stri = 'For  {}, the highest temperature will be {}℃ and the lowest temperature will be {}℃.'.format(oneDayData['date'], oneDayData['high'], oneDayData['low'])
     elif style == 'onlyClimate':
-        stri = oneDayData['date'] + ')气候:' + '\n' + oneDayData['type']
+        # stri = oneDayData['date'] + ')气候:' + '\n' + oneDayData['type']
+        stri = 'For {}, The weather will be "{}" at that time.'.format(oneDayData['date'], oneDayData['type'])
     elif style == 'onlyWind':
-        stri = oneDayData['date'] + ')风向风力:' + '\n' + (oneDayData['fx'] if 'fx' in oneDayData else oneDayData['fengxiang']) + ', ' + re.search(r'(?<=CDATA\[).*(?=\]\])', (oneDayData['fl'] if 'fl' in oneDayData else oneDayData['fengli'])).group()
+        stri = 'For {}, The strength of wind will be "{}", and the direction will be "{}" at that time.'.format(oneDayData['date'], (oneDayData['fx'] if 'fx' in oneDayData else oneDayData['fengxiang']), re.search(r'(?<=CDATA\[).*(?=\]\])', (oneDayData['fl'] if 'fl' in oneDayData else oneDayData['fengli'])).group())
     print(stri)
     print('translate stri:')
     print(transToEn(stri))
@@ -138,7 +148,33 @@ def parseOneDayStr(oneDayData, style):
 #parseOneDayStr(getOneDayData(getWeather("beijing"), 0), 'onlyClimate')
 #parseOneDayStr(getOneDayData(getWeather("shanghai"), 3), 'onlyWind')
 
-
+def getClimateDetail(oneDayData, msgStr):
+    flag = ''
+    if msgStr.find('rainy') != -1 or msgStr.find('sunny') != -1 or msgStr.find('cloudy') != -1 or msgStr.find('stormy') != -1:
+        if oneDayData['type'].find('雨') != -1:
+            flag = 'rainy' #有雨
+        elif oneDayData['type'].find('晴') != -1:
+            flag = 'sunny'
+        elif oneDayData['type'].find('云') != -1:
+            flag = 'cloudy'
+        elif oneDayData['type'].find('雷') != -1 or oneDayData['type'].find('大雨') != -1 or oneDayData['type'].find('暴雨') != -1:
+            flag = 'stormy'
+    elif msgStr.find('hot') != -1 or msgStr.find('cold') != -1 or msgStr.find('warm') != -1 or msgStr.find('cool') != -1:
+        if int(re.search(r'-?\d+', oneDayData['high']).group()) > 30:
+            flag = 'hot' #有雨
+        elif int(re.search(r'-?\d+', oneDayData['low']).group()) > 20 and int(re.search(r'-?\d+', oneDayData['high']).group()) <= 30:
+            flag = 'warm'
+        elif int(re.search(r'-?\d+', oneDayData['low']).group()) > 10 and int(re.search(r'-?\d+', oneDayData['high']).group()) <= 20:
+            flag = 'cool'
+        elif int(re.search(r'-?\d+', oneDayData['high']).group()) <= 10:
+            flag = 'cold'
+    elif msgStr.find('windy') != -1 or msgStr.find('storm') != -1:
+        if int(re.search(r'\d+', re.search(r'(?<=CDATA\[).*(?=\]\])', (oneDayData['fl'] if 'fl' in oneDayData else oneDayData['fengli'])).group()).group()) > 7:
+            flag = 'storm'
+        else:
+            flag = 'windy'
+    return flag
+    
 
 #print('------start:')
 #print(interpreter.parse("What's the weather like in Shenzhen today?"))
@@ -152,7 +188,18 @@ def parseMsg(interpreter):
 
     #interpreter = {'intent': {'name': 'askWeather', 'confidence': 0.9988591247903451}, 'entities': [{'start': 27, 'end': 35, 'value': 'shenzhen', 'entity': 'city', 'confidence': 0.9973445414808976, 'extractor': 'CRFEntityExtractor'}, {'start': 41, 'end': 42, 'value': '?', 'entity': 'city', 'confidence': 0.6232087923091154, 'extractor': 'CRFEntityExtractor'}], 'intent_ranking': [{'name': 'askWeather', 'confidence': 0.9988591247903451}, {'name': 'greet', 'confidence': 0.0009235777715449384}, {'name': 'bye', 'confidence': 0.00011801225976564498}, {'name': 'negative', 'confidence': 9.855893707871175e-05}, {'name': 'affirmative', 'confidence': 7.262412655369333e-07}], 'text': "What's the weather like in Shenzhen today?"}
 
-    if ((interpreter['intent']['name'] == 'askWeather' and interpreter['intent']['confidence'] > 0.9) or globleDict['shouldContinue'] == True):
+    #if ((interpreter['intent']['name'] == 'askWeather' and interpreter['intent']['confidence'] > 0.9) or globleDict['shouldContinue'] == True):
+    if (interpreter['intent']['name'] == 'bye' or interpreter['intent']['name'] == 'affirmative'):
+        returnStr = ['Thanks to you~',
+                     'Bye~ Feel free to ask me anytime!',
+                     'Thanks for your asking.',
+                     'Bye~ I\'m glad to help you!'][random.randint(0, 3)] #1111 改过了
+        params.clear()
+    elif (interpreter['intent']['name'] == 'greet'):
+        returnStr = ['Hi~ I\'m your weather assistant!',
+                     'Hello, you can ask everything about the weather to me',
+                     'Nice to meet you, I\'m your weather assistant!'][random.randint(0, 2)] #1111 改过了
+    elif (interpreter['intent']['name'] == 'askWeather' or interpreter['intent']['name'] == 'giveTime'):
     
         print(interpreter['entities'])
         
@@ -166,18 +213,23 @@ def parseMsg(interpreter):
                 print('time!!!')
                 print(ent['value'])
                 print(params)
-            elif ent['entity'] == 'weathers' and ent['confidence'] > 0.1:
-                print('weathers!!!')
+            elif ent['entity'] == 'contents' and ent['confidence'] > 0.1:
+                print('contents!!!')
                 print(ent['value'])
-                params['weathers'] = ent['value']
+                params['contents'] = ent['value']
+            elif ent['entity'] == 'elements' and ent['confidence'] > 0.1:
+                print('elements!!!')
+                print(ent['value'])
+                params['elements'] = ent['value']
         
         #flag = 0 #0:可返回数据 1:需继续询问城市 2:需继续询问时间
-        returnStr = 'None'
+        returnStr = 'None' ####
         #for param in params:
         if 'city' in params and 'time' in params: #存在城市 且 存在时间
-            globleDict['shouldContinue'] = False
-            if 'weathers' in params: #问的是具体的天气
-                print('has weathers!!!')
+            #globleDict['shouldContinue'] = False
+                
+            if False:
+                print('111')
             else: #问的是天气情况
                 day = 0
                 if params['time'].lower() == 'yesterday':
@@ -196,19 +248,66 @@ def parseMsg(interpreter):
                     day = 404 #超过可查询的时间
                     
                 if day == 404:
-                    returnStr: '当前时间无法查询，请重试。'
+                    returnStr = '当前时间无法查询，请重试。'
                 else:
-                    returnStr = parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'all')
-                    params.clear()
+                    if 'elements' in params:
+                        if 'contents' in params:
+                            del params['contents'] 
+                        print('has elements!!!')
+                        if params['elements'].find('temperature') != -1:
+                            returnStr = parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'onlyTemperature')
+                        elif params['elements'].find('wind') != -1:
+                            returnStr = parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'onlyWind')
+                        elif params['elements'].find('ultraviolet') != -1:
+                            returnStr = 'Sorry, I can\’t find the data of ultraviolet rays. Try to ask me something else.'
+                        elif params['elements'].find('humidity') != -1:
+                            returnStr = 'Sorry, I can\’t find the data of humidity. Try to ask me something else.'
+                    elif 'contents' in params: #问的是具体的天气
+                        if 'elements' in params:
+                            del params['elements'] 
+                        print('has contents!!!')
+                        #if params['contents'].find('雨') != -1:
+                        ###################todo
+                        #returnStr = params['time'].lower() + ' is ' + getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents']) + ':\n' + parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'onlyClimate')
+                        if params['contents'] == 'cold' or params['contents'] == 'cool' or params['contents'] == 'warm' or params['contents'] == 'hot':
+                            returnStr = ('Yes. ' if params['contents'] == getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents']) else 'No. ') + parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'onlyTemperature') + 'That is a {} day. '.format(getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents']))
+                        elif params['contents'] == 'sunny' or params['contents'] == 'cloudy' or params['contents'] == 'rainy' or params['contents'] == 'stormy':
+                            returnStr = ('Yes. ' if params['contents'] == getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents']) else 'No. ') + parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'onlyClimate') + 'That is a {} day. '.format(getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents'])) + ('Remember to take your umbrella~' if (getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents']) == 'rainy' or getClimateDetail(getOneDayData(getWeather(params['city']), day), params['contents']) == 'stormy') else '')
+                        #else:
+                            #returnStr = '没有'
+                            #returnStr = params['time'].lower() + ' not have rain\n' + parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'onlyClimate')
+                    else:
+                        if 'contents' in params:
+                            del params['contents'] 
+                        if 'elements' in params:
+                            del params['elements'] 
+                        returnStr = parseOneDayStr(getOneDayData(getWeather(params['city']), day), 'all')
+                    #params.clear()
         elif 'city' in params: #只存在城市，不存在时间
             #flag = 2
-            returnStr = 'What time?'
+            returnStr = ['What time?','At what time?','Uh...So what\'s the time?'][random.randint(0, 2)] ##改过了
             globleDict['shouldContinue'] = True
         elif 'time' in params:
             #flag = 1
-            returnStr = 'What city?'
+            returnStr = ['Which city?',
+                         'Of which city?',
+                         'Uh...So what\'s the city?',
+                         'Could you tell me which city are you talking about?'][random.randint(0, 3)]  ##改过了
             globleDict['shouldContinue'] = True
-        return returnStr
+        else:
+            if interpreter['intent']['name'] == 'askWeather':
+                returnStr = ['What time?','At what time?','Uh...So what\'s the time?'][random.randint(0, 2)] ##改过了
+            else:
+                returnStr = ['Sorry, I can not understand what you say.',
+                             'Sorry, please ask me something about weather.',
+                             'Try to ask me something about weather',
+                             'Sorry I can\'t understand you, i\'m a weather assistant.'][random.randint(0, 3)] # 我是一个天气助手。 改过了
+    else:
+        returnStr = ['Sorry, I can not understand what you say.',
+                     'Sorry, please ask me something about weather.',
+                     'Try to ask me something about weather',
+                     'Sorry I can\'t understand you, i\'m a weather assistant.'][random.randint(0, 3)]
+    return returnStr
             
                         
 
